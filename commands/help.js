692083635ff2847ba462ced0b0de6e5e17bab1d0
Norbik1004 <71138014+Norbik1004@users.noreply.github.com>
2020-11-30 10:13:29 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,12 +11,12 @@ module.exports = {
         .setTitle("**COMMANDS!**")
         .setDescription('Below, you can find my commands. My prefix is: **>**.')
         .addFields(
-	    	{ name: '>announce (text)', value: 'Announces someting.', inline: false },
-		    { name: '>help', value: 'Shows the list of commands.', inline: false },
+	    { name: '>announce (text)', value: 'Announces someting.', inline: false },
+	    { name: '>help', value: 'Shows the list of commands.', inline: false },
             { name: '>embed (text)', value: 'Sends and embed.', inline: false },
             { name: '>info', value: 'Shows imnportant information.', inline: false },
-        	{ name: '>avatar', value: 'Sends avatar link.', inline: false },
-		    { name: '>user', value: 'Shows your information.', inline: false },
+            { name: '>avatar', value: 'Sends avatar link.', inline: false },
+	    { name: '>user', value: 'Shows your information.', inline: false },
             { name: '>kick @user', value: 'Kicks user.', inline: false },
             { name: '>ban @user', value: 'Bans user.', inline: false },
 	    )
