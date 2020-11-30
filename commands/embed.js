5890bc6b6fc7750a1b636a149730a04eb4fc8e56
Norbik1004 <71138014+Norbik1004@users.noreply.github.com>
2020-11-30 10:08:33 +0100

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -2,14 +2,14 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: 'embed',
-    description: "used to create a embed",
+    description: "used to create an embed",
     execute(message, args){   
             let desc = args.slice(0).join(' ');
         
             const Discord = require("discord.js");
             const client = new Discord.Client();
         	const embed = new MessageEmbed()
-            .setTitle(desc)
+            .setDescription(desc)
         	.setFooter(`Requested by: ${message.author.tag}`, (message.author.displayAvatarURL()))
             .setColor('#1fff00');
             embed.setTimestamp()
