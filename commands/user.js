00726e074b951de15b71a72716be60c0173f6b29
Norbik1004 <71138014+Norbik1004@users.noreply.github.com>
2020-11-30 10:14:54 +0100

    Update user.js

--- commands/user.js
+++ commands/user.js
@@ -8,16 +8,16 @@ module.exports = {
         const client = new Discord.Client();
         const embed = new MessageEmbed()
         .setColor('#1fff00')
-	    .setTitle('**YOUR INFOMATION!**')
-          .setDescription('Your informationb:')
-	    .setThumbnail(message.author.displayAvatarURL())
-	    .addFields(
-	    	{ name: 'Bot:', value: `${message.author.bot}`, inline: false },
-		    { name: 'Your tag:', value: `${message.author.tag}`, inline: false },
+	.setTitle('**YOUR INFOMATION!**')
+        .setDescription('Your informationb:')
+	.setThumbnail(message.author.displayAvatarURL())
+	.addFields(
+	    { name: 'Bot:', value: `${message.author.bot}`, inline: false },
+	    { name: 'Your tag:', value: `${message.author.tag}`, inline: false },
             { name: 'When did you create your account and timezone:', value: `${message.author.createdAt}`, inline: false },
             { name: 'ID:', value: `${message.author.id}`, inline: false },
 	    )
-	    embed.setTimestamp()  
+	embed.setTimestamp()  
         .setFooter(`Requested by: ${message.author.tag}`, (message.author.displayAvatarURL()))
         message.channel.send(embed);
     }
