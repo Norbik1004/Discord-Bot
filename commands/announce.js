fc06132ed8d3b42addc4f9dbd6ce8e1d0f0ef7f5
Norbik1004 <71138014+Norbik1004@users.noreply.github.com>
2020-11-30 10:09:10 +0100

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -4,13 +4,13 @@ module.exports = {
     name: 'announce',
     description: "announce a announcement!",
     execute(message, args){
-            let msg = args.slice(0).join(' ');
+            let desc = args.slice(0).join(' ');
         
             const Discord = require("discord.js");
        		const client = new Discord.Client();
     	    const embed = new MessageEmbed()
      	    .setTitle("**ANNOUNCEMENT!**")
-            .setDescription(msg)
+            .setDescription(desc)
      	    .setFooter(`Requested by: ${message.author.tag}`, (message.author.displayAvatarURL()))
             .setColor("#ff1d00");
             embed.setTimestamp()
